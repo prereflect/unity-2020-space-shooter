f165524acf6181f0e24b9ce9fc7d4088f95d7235
Toby Woller <tobywoller@gmail.com>
2021-06-07 10:54:40 -0700

    Controlling the Player p 133

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool MouseLook = true;
+    public string HorzAxis = "Horizontal";
+    public string VertAxis = "Verticle";
+    public string FireAxis = "Fire1";
+    public float MaxSpeed = 5f;
+
+    private Rigidbody ThisBody = null;
+
+    void Awake()
+    {
+        ThisBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float Horz = Input.GetAxis(HorzAxis);
+        float Vert = Input.GetAxis(VertAxis);
+        Vector3 MoveDirection = new Vector3(Horz, 0.0f, Vert);
+        ThisBody.AddForce(MoveDirection.normalized * MaxSpeed);
+		ThisBody.velocity = new Vector3(Mathf.Clamp(ThisBody.velocity.x, -MaxSpeed, MaxSpeed),
+		                                Mathf.Clamp(ThisBody.velocity.y, -MaxSpeed, MaxSpeed),
+		                                Mathf.Clamp(ThisBody.velocity.z, -MaxSpeed, MaxSpeed));
+          
+		if(MouseLook)
+		{
+			Vector3 MousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+			MousePosWorld = new Vector3(MousePosWorld.x, 0.0f, MousePosWorld.z);
+			
+			Vector3 LookDirection = MousePosWorld - transform.position;
+			
+			transform.localRotation = Quaternion.LookRotation(LookDirection.normalized,Vector3.up);
+		}
+    }
+}
