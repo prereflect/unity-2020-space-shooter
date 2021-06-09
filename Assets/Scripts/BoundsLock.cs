3df842b0ef2bb84221c68fccff623c67be4e9e52
Toby Woller <tobywoller@gmail.com>
2021-06-09 14:09:08 -0700

    Creating a Health Component p 145

--- Assets/Scripts/BoundsLock.cs
+++ Assets/Scripts/BoundsLock.cs
@@ -6,16 +6,19 @@ public class BoundsLock : MonoBehaviour
 
 	void LateUpdate () 
 	{
-		transform.position = new Vector3(Mathf.Clamp(transform.position.x, levelBounds.xMin, levelBounds.xMax),
-											 transform.position.y,
-		                                     Mathf.Clamp(transform.position.z, levelBounds.yMin, levelBounds.yMax));
+		transform.position = new Vector3
+			(Mathf.Clamp(transform.position.x, levelBounds.xMin, levelBounds.xMax),
+			transform.position.y,
+			Mathf.Clamp(transform.position.z, levelBounds.yMin, levelBounds.yMax));
 	}
 
     void OnDrawGizmosSelected()
     {
 	    const int cubeDepth = 1;
-        Vector3 boundsCenter = new Vector3(levelBounds.xMin + levelBounds.width * 0.5f, 0, levelBounds.yMin + levelBounds.height * 0.5f);
-		Vector3 boundsHeight = new Vector3(levelBounds.width, cubeDepth, levelBounds.height);
-		Gizmos.DrawWireCube(boundsCenter, boundsHeight);    
+        Vector3 boundsCenter = new Vector3(
+            levelBounds.xMin + levelBounds.width * 0.5f, 0, levelBounds.yMin + levelBounds.height * 0.5f);
+			Vector3 boundsHeight = new Vector3
+				(levelBounds.width, cubeDepth, levelBounds.height);
+				Gizmos.DrawWireCube(boundsCenter, boundsHeight);    
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	public GameObject DeathParticlesPrefab = null;
+	public bool ShouldDestroyOnDeath = true;
+	[SerializeField] private float _HealthPoints = 100f;
+
+	public float HealthPoints
+	{
+		get
+		{
+			return _HealthPoints;
+		}
+
+		set
+		{
+			_HealthPoints = value;
+
+			if(_HealthPoints <= 0)
+			{
+				SendMessage("Die", SendMessageOptions.DontRequireReceiver);
+				if (DeathParticlesPrefab != null)
+				{
+					Instantiate(DeathParticlesPrefab, transform.position, transform.rotation);
+				}
+				if (ShouldDestroyOnDeath)
+				{
+					Destroy(gameObject);
+				}
+			}
+		}
+	}
+}
